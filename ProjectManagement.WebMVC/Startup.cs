e8a94b7a4bd1dc4262e448762743344ae4ace0f6
Kyle Johnson <59038857+kylejohnson1985@users.noreply.github.com>
2020-06-23 19:59:25 -0400

    Add project files.

---
+++ ProjectManagement.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectManagement.WebMVC.Startup))]
+namespace ProjectManagement.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
