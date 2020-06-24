4f9e6fd84d2ad595ce6e12f0e73b80379aa44243
Kyle Johnson <59038857+kylejohnson1985@users.noreply.github.com>
2020-06-23 20:12:28 -0400

    Added Entities

--- ProjectManagement.Data/Entities/Employee.cs
+++ ProjectManagement.Data/Entities/Employee.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,11 @@ namespace ProjectManagement.Data.Entities
 {
     public class Employee
     {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
     }
 }
