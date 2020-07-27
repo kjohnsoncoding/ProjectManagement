07df5131d059f6571d895bde554c4f2cd3c356f8
Kyle Johnson <kylejohnson1985@live.com>
2020-07-27 19:46:51 -0400

    Changed registration page

--- ProjectManagement.WebMVC/Models/AccountViewModels.cs
+++ ProjectManagement.WebMVC/Models/AccountViewModels.cs
@@ -65,22 +65,22 @@ namespace ProjectManagement.WebMVC.Models
 
     public class RegisterViewModel
     {
-        //[Required]
-        //[Display(Name = "First Name")]
-        //public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
 
-        //[Required]
-        //[Display(Name = "Last Name")]
-        //public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
 
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        //[Display(Name = "UserName")]
-        //[StringLength(50)]
-        //public string UserName { get; set; }
+        [Display(Name = "UserName")]
+        [StringLength(50)]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
